5758cb931530c796e0d128c91b17b0890b3e11e3
Doğan Kablan <mdogankablan@gmail.com>
2021-08-17 18:16:41 +0300

    update

---
+++ src/Components/ListItem/todoItem.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View,Text} from "react-native";
+
+const todo = props => {
+    return (
+        <View>
+            <Text>
+                TO DO COMPONENTS
+            </Text>
+        </View>
+    );
+};
+
+export default todo;
